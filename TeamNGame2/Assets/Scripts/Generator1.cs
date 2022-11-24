edb11f676d83b625aa1865010e66dbbab82874f6
Gilchrist <BG431@student.aru.ac.uk>
2022-11-24 20:34:44 +0000

    Generator code Functional, Functional storage code

--- TeamNGame2/Assets/Scripts/Generator1.cs
+++ TeamNGame2/Assets/Scripts/Generator1.cs
@@ -4,36 +4,48 @@ using UnityEngine;
 
 public class Generator1 : MonoBehaviour
 {
+
+    //GameObjects
+    public GameObject camera;
     public GameObject generator;
+    public Storage storage;
+
+    //Upgrade Multiplier
     public int multiplier = 1;
-    public bool active;
-    
+
+    //Timer Variables
+    private float nextActionTime = 0.0f;
+    public float period = 1f;
+
     Game gameScript;
+    
 
     WaitForSeconds waitForSeconds = new WaitForSeconds(1f);
 
     void Start()
     {
-        gameScript = GetComponent<Game>();
+        storage = storage.GetComponent<Storage>();
+        gameScript = camera.GetComponent<Game>();
     }
 
     void Update()
     {
-        if (generator.activeSelf)
-        {            
-            Go();
-        }
-    }
+        if (generator.activeSelf)                                                   //Runs check if Generator is Active
+        {                                                                           
+            if (storage.quantity > 0)                                               //If there is any objects in storage, Runs main generator function
+            {
 
-    IEnumerator Go()
-    {
-        while (true)
-        {
+                if (Time.time > nextActionTime)
+                {
+                    nextActionTime += period;                                        
+
+                    storage.quantity--;                                             //Takes one away from storage once used
+
+                    gameScript.points = gameScript.points + multiplier;             //Adds points in relation to Multiplier (Upgrades)
+                    gameScript.ShowPoints();
+                }
+            }
             
-            gameScript.points = (gameScript.points + 1) * multiplier;
-            gameScript.ShowPoints();
-            
-            yield return waitForSeconds;
         }
-    }
+    }  
 }
---
+++ TeamNGame2/Assets/Scripts/Storage.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Storage : MonoBehaviour
+{
+    public float quantity;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
