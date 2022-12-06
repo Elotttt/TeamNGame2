89b057d8e8bcff01d3389cde0eda53f57eb473d1
Gilchrist <BG431@student.aru.ac.uk>
2022-12-06 11:29:05 +0000

    Mail DeliveryV2

--- TeamNGame2/Assets/Scripts/Generator1.cs
+++ TeamNGame2/Assets/Scripts/Generator1.cs
@@ -50,4 +50,6 @@ public class Generator1 : MonoBehaviour
             
         }
     }  
+    
+    
 }
--- TeamNGame2/Assets/Scripts/MailDelivery.cs
+++ TeamNGame2/Assets/Scripts/MailDelivery.cs
@@ -29,4 +29,6 @@ public class MailDelivery : MonoBehaviour
             storageQuantity = storageQuantity + storageTopUp;
         }
     }
+
+    
 }
