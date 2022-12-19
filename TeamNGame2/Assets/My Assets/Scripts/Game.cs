4a65da4fd7b61b4ff7f9b32c614837961ef942ec
Elotttt <elliot@jrfry.co.uk>
2022-12-19 12:19:52 +0000

    FIN
    Added updated idle scene. Fixed point bug.

--- TeamNGame2/Assets/My Assets/Scripts/Game.cs
+++ TeamNGame2/Assets/My Assets/Scripts/Game.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Game : MonoBehaviour
 {
     public float points;
 
+    public TMP_Text pointText;
+
     void Start()
     {
         points = VariableTransfer.points;
+        pointText.text = points.ToString();
     }
 
     // Update is called once per frame
@@ -18,17 +22,16 @@ public class Game : MonoBehaviour
         if (Input.GetButtonDown("Fire1")) //On click, adds point
         {
             points++;
-            Debug.Log(points);
+            pointText.text = points.ToString();
             VariableTransfer.points = points;
         }
 
         if (Input.GetButtonDown("Fire2"))
         {
+            VariableTransfer.points = points;
             SceneManager.LoadScene("Game");
         }
+        VariableTransfer.points = points;
     }
-    public void ShowPoints()
-    {
-        Debug.Log(points);
-    }
+    
 }
--- TeamNGame2/Assets/My Assets/Scripts/Shipper.cs
+++ TeamNGame2/Assets/My Assets/Scripts/Shipper.cs
@@ -4,40 +4,33 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class Generator1 : MonoBehaviour
+public class Shipper : MonoBehaviour
 {
     //Text variables
     public TMP_Text pointText;
 
-    public bool isActive;
-
-    //GameObjects
-    public GameObject camera;
-    public GameObject generator;
-   
     //Script variables
-    public MailDelivery storageOne;
+    //public MailDelivery mailDelivery;
+    public Storage storageOne;
     public Storage storageTwo;
-    Game gameScript;
+    public Game gameScript;
 
     //Upgrade Multiplier
     public float multiplier = 1;                  //this is the variable for shop upgrades (plus by multiplier after generate speed)
 
     //Timer Variables
-    float nextActionTime = 0.0f;
+    public float nextActionTime = 0.0f;
     public float generateSpeed = 1f;
 
 
-    
+
 
     /// <summary>
     /// Initialises all variables with data from other scripts
     /// </summary>
     void Start()
-    {       
-        storageOne = storageOne.GetComponent<MailDelivery>();
-        storageTwo = storageTwo.GetComponent<Storage>();
-        gameScript = camera.GetComponent<Game>();
+    {
+
     }
 
 
@@ -47,8 +40,8 @@ public class Generator1 : MonoBehaviour
     void Update()
     {
         MainGenerator();
-    }  
-    
+    }
+
 
     /// <summary>
     /// This embodies the generator functionality
@@ -57,24 +50,22 @@ public class Generator1 : MonoBehaviour
     /// </summary>
     void MainGenerator()
     {
-        if (generator.activeSelf)                                                   //Runs check if Generator is Active and If there is any objects in storage, Runs main generator function
+        if (storageOne.quantity > 0)                                                   //Runs check if Generator is Active and If there is any objects in storage, Runs main generator function
         {
 
             if (Time.time > nextActionTime)                                                         //
             {
                 nextActionTime += generateSpeed;
 
-                storageOne.storageQuantity--;                                             //Takes one away from storage once used
+                storageOne.quantity--;                                             //Takes one away from storage once used
                 storageTwo.quantity++;
 
+                pointText.text = gameScript.points.ToString();                                              //displays points quantity to the ui                       
+
                 gameScript.points = gameScript.points + multiplier;                      //Adds points in relation to Multiplier (Upgrades)
-
-                pointText.text = gameScript.points.ToString();                                              //displays points quantity to the ui                                      
+                
             }
-
-
-
         }
     }
-    
+
 }
